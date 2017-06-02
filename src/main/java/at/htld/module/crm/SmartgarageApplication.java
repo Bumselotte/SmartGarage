fd58d979d372c5dc4df8a10bead5e1e0c5d778b3
elias <elias.blum998@gmail.com>
2017-06-02 09:54:33 +0200

    Initial commit

---
+++ src/main/java/at/htld/module/crm/SmartgarageApplication.java
@@ -0,0 +1,12 @@
+package at.htld.module.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgarageApplication.class, args);
+	}
+}
---
+++ src/test/java/at/htld/module/crm/SmartgarageApplicationTests.java
@@ -0,0 +1,16 @@
+package at.htld.module.crm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartgarageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
